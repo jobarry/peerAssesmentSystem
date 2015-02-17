c491b0b2aa4039e946018604e63e64c87e0a1454
jobarry <jobarry@tcd.ie>
2015-02-17 15:12:40 +0000

    test php

---
+++ WebContent/PHP/newfile.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname ="studentDB";
+$port = "3306";
+
+$conn = new mysqli($servername,$username,$password,$dbname,$port);
+
+if($conn -> connect_error){
+	
+	die("Connection failed:".$conn->connect_error);
+	
+}
+
+$sql = "INSERT INTO Forum (idForums,title) VALUES (14,'BOWEN')";
+
+if ($conn->query($sql)===true) {
+	// output data of each row
+	echo "Selection complete";
+} else {
+	echo "Error:". $sql."<br>". $conn -> error;
+}
+$conn->close();
+
+?>
