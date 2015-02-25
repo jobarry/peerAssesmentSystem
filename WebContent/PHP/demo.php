e73f9ad7c8d0991343ac4c6e45dabe3312d265a0
jobarry <jobarry@tcd.ie>
2015-02-25 13:48:10 +0000

    JohnsBranch

---
+++ WebContent/PHP/demo.php
@@ -0,0 +1,4 @@
+<form action="newfile.php" method="post"/>
+<p>Input 1: <input type="text" name="input1"/><input type="text" name="input2"/></p>
+<input type="submit" value="Submit" />
+</form>
--- WebContent/PHP/newfile.php
+++ WebContent/PHP/newfile.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname ="studentDB";
-$port = "3306";
+$port = "3308";
 
 $conn = new mysqli($servername,$username,$password,$dbname,$port);
 
@@ -12,15 +12,27 @@ if($conn -> connect_error){
 	die("Connection failed:".$conn->connect_error);
 	
 }
-//
-$sql = "INSERT INTO Forum (idForums,title) VALUES (14,'BOWEN')";
 
-if ($conn->query($sql)===true) {
-	// output data of each row
-	echo "Selection complete";
-} else {
-	echo "Error:". $sql."<br>". $conn -> error;
+if($conn){
+	echo "Success connecting to DB";
 }
+
+// $value = $_POST['input1'];
+// $value2 = $_POST['input2'];
+
+//$sql = "INSERT INTO Student (fName,lName, Sex, idStudent, idStudentGroup) VALUES ('John','Barry','M','123','2')";
+		
+		
+		
+//
+// $sql = "INSERT INTO Forum (idForums,title) VALUES ('55','John')";
+
+// if ($conn->query($sql)===true) {
+// 	// output data of each row
+// 	echo "Selection complete";
+// } else {
+// 	echo "Error:". $sql."<br>". $conn -> error;
+// }
 $conn->close();
 
 ?>
